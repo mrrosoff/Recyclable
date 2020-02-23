63fc43ad8c7e4e2f88df6de824d593860ea79af3
mrosoff <mrosoff@ramscolostate.edu>
2020-02-23 01:33:23 -0700

    UI Buffs

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,9 +5,9 @@ import DetailsPage from './pages/DetailsPage';
 import CameraComponent from "./CameraComponent";
 
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import { Container, Grid } from '@material-ui/core';
+import { Box, Container, Grid } from '@material-ui/core';
 
-const useStyles = makeStyles({ root: { position: 'fixed', bottom: 0, width: '100%' } });
+const useStyles = makeStyles({ page: { marginTop: 80, height: "85vh"} });
 
 const Layout = props => {
 
@@ -15,14 +15,18 @@ const Layout = props => {
     const [page, setPage] = useState(0);
     const [objectDetails, setObjectDetails] = useState({'name': '', 'recyclable': null});
 
+    const classes = useStyles();
+
     return(
         <>
             <Container>
-                <Grid container justify={"center"} alignContent={"center"} alignItems={"center"} direction={"column"}>
-                    <Grid item>
-                        { page ? <DetailsPage dataUri={picUri}/> : <CameraComponent setPicUri={setPicUri} setPage={setPage} /> }
+                <Box className={classes.page}>
+                    <Grid container justify={"center"} alignContent={"center"} alignItems={"center"} direction={"column"}>
+                        <Grid item>
+                            { page ? <DetailsPage dataUri={picUri}/> : <CameraComponent setPicUri={setPicUri} setPage={setPage} /> }
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Box>
             </Container>
             <Navigation page={page} setPage={setPage} />
         </>
