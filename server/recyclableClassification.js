575a81436af98cc5c38ad39136bb635dda2a5baf
David Hallstrom <dhallstr@rams.colostate.edu>
2020-02-23 10:08:58 -0700

    add database of items

--- server/recyclableClassification.js
+++ server/recyclableClassification.js
@@ -1,13 +1,27 @@
+const recycle_data = [
+    {displayName: 'cheezeit', actualName: 'Cheez-It wrapper', recyclable: false},
+    {displayName: 'goldfish', actualName: 'Goldfish wrapper', recyclable: false},
+    {displayName: 'lays', actualName: 'Lays wrapper', recyclable: false},
+    {displayName: 'coke', actualName: 'Coca-Cola can', recyclable: true},
+    {displayName: 'dietcoke', actualName: 'Diet Coca-Cola can', recyclable: true},
+    {displayName: 'sprite', actualName: 'Sprite can', recyclable: true},
+    {displayName: 'arizonatea', actualName: 'Arizona Tea can', recyclable: true},
+    {displayName: 'drpepper', actualName: 'Dr Pepper can', recyclable: true},
+    {displayName: 'gatorade', actualName: 'Gatorade can', recyclable: true},
+    {displayName: 'redbull', actualName: 'Red Bull can', recyclable: true},
+    {displayName: 'welch', actualName: 'Welch\'s wrapper', recyclable: false},
+    {displayName: 'mnms', actualName: 'M&M\'s wrapper', recyclable: false}
+];
+
 const classifyProduct = r => {
     console.log(r);
     let name = "unknown", recyclable = null;
     if (r.length && r[0].classification.score > 0.7) {
         name = r[0].displayName;
-        if (name === 'cheezeit') {
-            recyclable = false;
-        }
-        else if (name === 'coke') {
-            recyclable = true;
+        entry = recycle_data.filter(a => a.displayName === name);
+        if (entry.length === 1) {
+            name = entry.actualName;
+            recyclable = entry.recyclable;
         }
     }
     return {name: name, recyclable: recyclable, original: r};
--- src/utils/resizeImage.js
+++ src/utils/resizeImage.js
@@ -4,31 +4,42 @@ const resizeImage = (uri, maxDim, callback) => {
     reader.readAsDataURL(new Blob([uri]));
     reader.onloadend = function (e) {
         const contents = e.target.result;
-        const memoryImg = document.createElement('img');
-        memoryImg.src = contents;
-        const width = memoryImg.width;
-        const height = memoryImg.height;
+        console.log('contents', contents);
+        const memoryImg = new Image();
 
-        const newScale = maxDim / Math.min(width, height);
-        const newWidth = parseInt(width * newScale);
-        const newHeight = parseInt(height * newScale);
+        memoryImg.onload = function() {
+            const width = memoryImg.width;
+            const height = memoryImg.height;
 
-        var sourceImage = new Image();
+            console.log('oldWidth', width);
+            console.log('oldHeight', height);
 
-        sourceImage.onload = function() {
-            // Create a canvas with the desired dimensions
-            const canvas = document.createElement("canvas");
-            canvas.width = newWidth;
-            canvas.height = newHeight;
+            const newScale = maxDim / Math.min(width, height);
+            const newWidth = parseInt(width * newScale);
+            const newHeight = parseInt(height * newScale);
 
-            // Scale and draw the source image to the canvas
-            canvas.getContext("2d").drawImage(sourceImage, 0, 0, newWidth, newHeight);
+            console.log('newWidth', newWidth);
+            console.log('newHeight', newHeight);
 
-            // Convert the canvas to a data URL in PNG format
-            callback(canvas.toDataURL());
+            var sourceImage = new Image();
+
+            sourceImage.onload = function () {
+                // Create a canvas with the desired dimensions
+                const canvas = document.createElement("canvas");
+                canvas.width = newWidth;
+                canvas.height = newHeight;
+
+                // Scale and draw the source image to the canvas
+                canvas.getContext("2d").drawImage(sourceImage, 0, 0, newWidth, newHeight);
+
+                // Convert the canvas to a data URL in PNG format
+                callback(canvas.toDataURL());
+            }
+
+            sourceImage.src = uri;
         }
+        memoryImg.src = contents;
 
-        sourceImage.src = uri;
     }*/
 };
 
