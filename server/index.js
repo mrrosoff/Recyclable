cd6b79d982842c672bced7ef9ec7689904f4150f
Ben Gillett <bnjmn.gillett@gmail.com>
2020-02-22 21:51:57 -0700

    updated cert path

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const express = require('express');
 const app = express();
 
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/recyclable.tech/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/recyclable.tech/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/recyclable.tech/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('../recyclable.tech/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('../recyclable.tech/cert.pem', 'utf8');
+const ca = fs.readFileSync('../recyclable.tech/chain.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
