0a64e6ce02d2315b8b2c004077b413f60c87a525
David Hallstrom <dhallstr@rams.colostate.edu>
2020-02-23 00:57:23 -0700

    Adjusted picture size

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,17 +7,22 @@ import { Button, Box, Container, Grid } from '@material-ui/core';
 import DetailsPage from './pages/DetailsPage';
 
 import { sendServerRequestWithBody } from "../api/restfulAPI";
+import useWindowDimensions from "../utils/windowDimensions";
 
 const videoConstraints = {
     facingMode: "environment"
 };
 
-const CameraPage = () => {
+const CameraPage = props => {
     const webcamRef = React.useRef(null);
+    const { height, width } = useWindowDimensions();
 
     const capture = React.useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
+        props.setPicUri(imageSrc);
+        props.setPage(1);
         console.log(imageSrc);
+        console.log(webcamRef.current);
     }, [webcamRef]);
 
     return (
@@ -32,7 +37,7 @@ const CameraPage = () => {
                     <Webcam
                         audio={false}
                         ref={webcamRef}
-                        height={"600"}
+                        height={parseInt(height*0.7)}
                         screenshotFormat="image/jpeg"
                         videoConstraints={videoConstraints}
                     />
@@ -63,13 +68,14 @@ const Layout = props => {
 
     const [picUri, setPicUri] = useState('');
     const [page, setPage] = useState(0);
+    const [objectDetails, setObjectDetails] = useState({'name': '', 'recyclable': null});
 
     return(
         <Container>
             <Grid container justify={"center"} alignContent={"center"} alignItems={"center"} direction={"column"}>
                 <Grid item>
                     <Box height={"90vh"}>
-                        { page ? <DetailsPage dataUri={picUri}/> : <CameraPage setPicUri={setPicUri} setPage={setPage} /> }
+                        { page ? <DetailsPage dataUri={picUri} objectDetails={objectDetails} /> : <CameraPage setPicUri={setPicUri} setPage={setPage} /> }
                     </Box>
                 </Grid>
                 <Grid item>
