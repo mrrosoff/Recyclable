d95a7cae4727db5caaed38353daddf1bf46c5959
Max Rosoff <me@maxrosoff.com>
2021-06-09 10:56:36 -0700

    update packages so I stop getting Github email garbage

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,59 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const outputDirectory = "dist";
+const output = "dist";
 
 module.exports = {
-	entry: ["@babel/polyfill", "./client/index.js"],
-	devServer: { port: 3000, open: true, hot: true, proxy: { "/api/*": "http://localhost:8080" } },
-	devtool: 'cheap-source-map',
+	entry: "./client/index.js",
+	devServer: {
+		contentBase: path.resolve(__dirname, output),
+		port: 3000,
+		open: true,
+		hot: true,
+		proxy: { "/api/*": "http://localhost:8080" }
+	},
+	devtool: "eval-source-map",
+	mode: process.env.NODE_ENV || "development",
 	module: {
 		rules: [
-			{test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/react'] }},
-			{ test: /\.css$/i, use: ['style-loader', 'css-loader']},
 			{
-				test: /\.s[ac]ss$/i, use:
-					[
-						{ loader: 'style-loader' },
-						{ loader: 'css-loader' },
-						{ loader: 'postcss-loader', options:
-								{
-									plugins: function()
-									{
-										return [require('precss'), require('autoprefixer')];
-									}
-								}
-						},
-						{ loader: 'sass-loader' }
-					]
+				test: /\.m?jsx?$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
 			},
-			{test: /\.(png|svg|jpe?g|gif)$/, loader: ['file-loader']}
+			{ test: /\.css$/i, use: ["style-loader", "css-loader"] },
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					{ loader: "style-loader" },
+					{ loader: "css-loader" },
+					{
+						loader: "postcss-loader",
+						options: {
+							postcssOptions: { plugins: ["autoprefixer"] }
+						}
+					},
+					{ loader: "sass-loader" }
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpe?g|gif|mp4|wav|mp3)$/i,
+				loader: "file-loader"
+			}
 		]
 	},
+	output: { filename: "bundle.js", path: path.join(__dirname, output) },
 	plugins: [
 		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({ template: "client/static/template/index.html", favicon: "client/static/template/favicon.ico"}),
-		new webpack.HotModuleReplacementPlugin()
+		new HtmlWebpackPlugin({
+			template: "client/static/template/index.html",
+			favicon: "client/static/template/favicon.ico",
+			title: "Recyclable"
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.DefinePlugin({
+			"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
+		})
 	],
-	output: { filename: "bundle.js", path: path.join(__dirname, outputDirectory) },
+	stats: "minimal"
 };
